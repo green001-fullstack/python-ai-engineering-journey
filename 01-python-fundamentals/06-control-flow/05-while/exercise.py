69f1b61e8a4764ca09ff08241e0acf351b138350
obolarinw <oladimejiemmanuel2015@gmail.com>
2026-07-22 12:09:29 +0100

    completed while statement

---
+++ 01-python-fundamentals/06-control-flow/05-while/exercise.py
@@ -0,0 +1,41 @@
+count = 1
+while count <= 10:
+    print(count)
+    count += 1
+
+
+
+number = 2
+while number <= 20:
+    print(number)
+    number += 2
+
+
+
+count = 10
+while count > 0:
+    print(count)
+    count -= 1
+
+
+
+count = 3
+while count > 0:
+    print(count)
+    count -= 1
+
+print("Done")
+
+Ans : 3,2,1, "Done"
+
+
+What is wrong with this code
+
+number = 1
+
+while number < 5:
+    print(number)
+
+Will it stop. Why or why not
+
+Ans: This code will run infinitely becasue it will always see and evaluate number to be true as it is not being updated
