dc62ea74f1576ef2a2eb8da9e354eff87a314749
Oladimeji Emmanuel <oladimejiemmanuel2015@gmail.com>
2026-08-15 13:35:18 +0100

    added symmetric matrix exercise file

--- 02-mathematics/02-linear-algebra/matrix/matrix_transpose.py
+++ 02-mathematics/02-linear-algebra/matrix/matrix_transpose.py
@@ -15,3 +15,30 @@ def transpose(matrix):
         result.append(new_row)
 
     return result
+
+    # List comprehension version
+    transpose = [ list(row) for row in zip(*matrix)]
+
+
+    # Numpy version
+
+    import numpy as np
+
+# Create a 3x3 matrix
+A = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])
+
+# Method 1: Using the .T attribute (most common)
+transpose_T = A.T
+
+# Method 2: Using the np.transpose() function
+transpose_func = np.transpose(A)
+
+print("Original Matrix A:")
+print(A)
+
+print("\nTransposed using A.T:")
+print(transpose_T)
--- 02-mathematics/02-linear-algebra/matrix/symmetric_matrix.py
+++ 02-mathematics/02-linear-algebra/matrix/symmetric_matrix.py
@@ -1,3 +1,5 @@
+# Pure python version
+
 def is_symmetric(matrix):
 
     rows = len(matrix)
@@ -13,3 +15,23 @@ def is_symmetric(matrix):
                 return False
 
     return True
+
+
+    A = [
+    [1, 2, 3],
+    [2, 5, 6],
+    [3, 6, 9]
+]
+
+print(is_symmetric(A))
+
+# Numpy version
+import numpy as np
+
+A = np.array([
+    [1, 2, 3],
+    [2, 5, 6],
+    [3, 6, 9]
+])
+
+print(np.array_equal(A, A.T))
